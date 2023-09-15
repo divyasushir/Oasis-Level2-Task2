fe3138ba174fa71e3be72d544214e63c77acc3d1
Divya Sushir <99466294+divyasushir@users.noreply.github.com>
2023-09-15 23:50:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// JavaScript code for the tribute website
+document.addEventListener('DOMContentLoaded', function() {
+    const quotesSection = document.getElementById('quotes');
+    quotesSection.addEventListener('click', function() {
+      alert('A.P.J. Abdul Kalam was an inspiration to many through his words and actions.');
+    });
+  });
